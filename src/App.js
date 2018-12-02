d1239aa92eb5d76fa7c1b7b9fe31332279c6c2ad
yadavdesh <bhardwajdesh@gmail.com>
2018-12-02 14:33:07 +0100

    api sink

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Venue } from './venue';
+import { Search } from './search';
+import 'whatwg-fetch';
 
 class App extends Component {
+
+  constructor() {
+
+    super();
+
+    this.state = {
+      venues: []
+    };
+
+  }
+
+  handleSubmit(query) {
+    this.getVenues(query);
+  }
+
+  componentDidMount() {
+    this.getVenues('Pubs');
+  }
+
+  getLocation(callback) {
+    navigator.geolocation.getCurrentPosition(function(location) {
+      callback(location.coords.latitude + ',' + location.coords.longitude)
+    })
+  }
+
+  getVenues(query) {
+
+    let setVenueState = this.setState.bind(this);
+
+    const venuesEndpoint = 'https://api.foursquare.com/v2/venues/explore?';
+
+    this.getLocation(function (latlong) {
+
+      const qs = {
+        client_id: '13J0BEBZY2UFW2IDD5YAESWL3OCDU0QZS1CXMF0NBVKCZENC',
+        client_secret: 'Y0CPZFJG5ORLABZ3GQOHJ1WOFFPCGDKJTFLRCODFUW2D4GB2',
+        limit: 3,
+        query: query,
+        v: '20170801',
+        ll: latlong
+      };
+
+      fetch(venuesEndpoint + new URLSearchParams(qs), {
+        method: 'GET'
+      }).then(response => response.json()).then(response => {
+        setVenueState({venues: response.response.groups[0].items});
+      });
+
+    });
+
+  }
+
   render() {
+
+    var venueList = this.state.venues.map((item, i) =>
+      <Venue key={i} name={item.venue.name}/> //Create a new "name attribute"
+    );
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <header>Lunch Places</header>
+        <Search onSubmit={(value)=>this.handleSubmit(value)}/>
+        <ul>
+          {venueList}
+        </ul>
       </div>
     );
   }
+
 }
 
 export default App;
